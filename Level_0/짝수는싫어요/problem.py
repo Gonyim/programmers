af4112bfe0886ebaec43d6ead28ccbf841021d29
gony <gwonyi4499@gmail.com>
2024-03-28 18:19:39 +0900

    update

---
+++ Level_0/짝수는싫어요/problem.py
@@ -0,0 +1,7 @@
+count = [0] * 4
+
+print(count)
+
+array = [1,1,2,2]
+
+print(max(array))
--- Level_2/최댓값과최솟값/problem.py
+++ Level_2/최댓값과최솟값/problem.py
@@ -1,9 +1,9 @@
-s = "-1 -3 -2 -4"
+s = "-1 3 -2 -4"
 
 # a = s.split()
 
 answer = ''
 answer = s.split()
-answer.sort()
-answer = answer[0],answer[-1]
-print(answer)
+answer.sort(reverse=True)
+# answer = answer[0],answer[-1]
+print(answer[0],answer[1])
