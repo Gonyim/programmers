fc56bd32de7d79eef14f80f522e11911d9b2cab1
gony <gwonyi4499@gmail.com>
2024-05-18 23:38:30 +0900

    fix

--- Level_0/모음제거/problem.py
+++ Level_0/모음제거/problem.py
@@ -1,12 +1,7 @@
-my_string = "bus"
-my_string.replace("u", "")
-
-print('bus')
-
 def solution(my_string):
-    my_string.replace("a", "")
-    my_string.replace("e", "")
-    my_string.replace("i", "")
-    my_string.replace("o", "")
-    my_string.replace("u", "")
+    my_string = my_string.replace("a", "")
+    my_string = my_string.replace("e", "")
+    my_string = my_string.replace("i", "")
+    my_string = my_string.replace("o", "")
+    my_string = my_string.replace("u", "")
     return my_string
