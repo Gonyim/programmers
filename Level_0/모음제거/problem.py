771600920eaa8f5618b1d0dfff60953705e64891
gony <gwonyi4499@gmail.com>
2024-05-18 23:34:39 +0900

    lv0_update

---
+++ Level_0/모음제거/problem.py
@@ -0,0 +1,12 @@
+my_string = "bus"
+my_string.replace("u", "")
+
+print('bus')
+
+def solution(my_string):
+    my_string.replace("a", "")
+    my_string.replace("e", "")
+    my_string.replace("i", "")
+    my_string.replace("o", "")
+    my_string.replace("u", "")
+    return my_string
