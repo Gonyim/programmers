1ce02c6130c942898845c3b775c471d6e3f88efd
Gony <gwonyi4499@gmail.com>
2024-06-15 01:45:01 +0900

    'fix'

--- Level_0/접미사인지 확인하기/problem.py
+++ Level_0/접미사인지 확인하기/problem.py
@@ -3,7 +3,7 @@ def solution(my_string, is_suffix):
     if len(is_suffix) > len(my_string):
         return 0
     else:
-        for i in range(1, len(is_suffix)):
+        for i in range(1, len(is_suffix)+1):
             if my_string[-i] == is_suffix[-i]:
                 answer = 1
             else:
