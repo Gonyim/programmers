b747396a277a85bf28ad47413fdee1e906b72e58
Gony <gwonyi4499@gmail.com>
2024-06-28 09:44:46 +0900

    'lv0_update'

---
+++ Level_0/ad제거하기/problem.py
@@ -0,0 +1,8 @@
+def solution(strArr):
+    answer = []
+    for i in range(0,len(strArr)):
+        if "ad" in strArr[i]:
+            pass
+        else:
+            answer.append(strArr[i])
+    return answer
